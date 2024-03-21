5f844a8d1a3818c4850be00a1ba427b2017c28e7
Danila Zakharchuk <zakharchukdanila@gmail.com>
2024-03-21 21:30:56 +0100

    task 10

--- square_gen/square_generator.py
+++ square_gen/square_generator.py
@@ -1,14 +1,16 @@
-import exception
+# from square_gen import exception
+from abc import ABC, abstractmethod
 
 
-class SquareGenerator:
+class SquareGenerator(ABC):
     @staticmethod
+    @abstractmethod
     def e_squares(start, end):
-        try:
-            if start >= end:
-                raise exception.InvalidBoundaryException
-            else:
-                return [x ** 2 for x in range(start, end + 1)]
-        except exception.InvalidBoundaryException:
-            print("Right bound is less than left")
         pass
+        # try:
+        #     if start >= end:
+        #         raise exception.InvalidBoundaryException
+        #     else:
+        #         return [x ** 2 for x in range(start, end + 1)]
+        # except exception.InvalidBoundaryException:
+        #     print("Right bound is less than left")
---
+++ tasks/task10.py
@@ -0,0 +1,3 @@
+from cubic_gen import cubic_generator
+
+print(cubic_generator.CubicGenerator.e_squares(1, 10))
